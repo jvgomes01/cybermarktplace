e546d31595056e4019599954687943b91101e1ce
Joao Gomes <gomesj.victor@yahoo.com.br>
2024-02-05 01:17:19 -0300

    Incorporar o projeto ao framework grails.

---
+++ src/test/groovy/cybermarketplace/TESTEControllerSpec.groovy
@@ -0,0 +1,18 @@
+package cybermarketplace
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class TESTEControllerSpec extends Specification implements ControllerUnitTest<TESTEController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
